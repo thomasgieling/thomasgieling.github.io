13aaeff5c7b47ce42fcc3bc28f419a0df6805894
Gieling, Thomas <Thomas.Gieling@adidas-group.com>
2015-10-16 08:58:23 +0200

    brand

--- app/js/test.js
+++ app/js/test.js
@@ -5,34 +5,32 @@ $(function() {
 	var li = $('<li/>').append(a);
    $('#testlinks').append(li);
 });
-<script type="text/javascript">
-	$(document).ready(function() {
+$(document).ready(function() {
+	
+	var actions = {
+		uk : "http://staging.store.adidasgroup.demandware.net/on/demandware.store/Sites-adidas-GB-Site/en_GB/URLBuilder-GetSEOConfig",
+		de : "http://staging.store.adidasgroup.demandware.net/on/demandware.store/Sites-adidas-DE-Site/de_DE/URLBuilder-GetSEOConfig",
+		nl : "http://staging.store.adidasgroup.demandware.net/on/demandware.store/Sites-adidas-NL-Site/nl_NL/URLBuilder-GetSEOConfig",
+		pl : "http://staging.store.adidasgroup.demandware.net/on/demandware.store/Sites-adidas-PL-Site/pl_PL/URLBuilder-GetSEOConfig"			
+	};		
+	
+	$("#urlBuilderForm").on("submit", function(e) {
 		
-		var actions = {
-			uk : "http://staging.store.adidasgroup.demandware.net/on/demandware.store/Sites-adidas-GB-Site/en_GB/URLBuilder-GetSEOConfig",
-			de : "http://staging.store.adidasgroup.demandware.net/on/demandware.store/Sites-adidas-DE-Site/de_DE/URLBuilder-GetSEOConfig",
-			nl : "http://staging.store.adidasgroup.demandware.net/on/demandware.store/Sites-adidas-NL-Site/nl_NL/URLBuilder-GetSEOConfig",
-			pl : "http://staging.store.adidasgroup.demandware.net/on/demandware.store/Sites-adidas-PL-Site/pl_PL/URLBuilder-GetSEOConfig"			
-		};		
-		
-		$("#urlBuilderForm").on("submit", function(e) {
-			
-			e && e.preventDefault();
-			var queryString = $(this).serialize();
-			var selected = $("select option:selected").val();
-			var url = actions[selected];
-			$.ajax({
-				url: url,
-				type: 'GET',
-				data: queryString,
-				success: function(response) {
-					$("#result").html(JSON.stringify(response));
-				},
-				error: function(xhr, textStatus) {
-					$("#result").html(textStatus + "!");
-				}
-			});
-		});			
+		e && e.preventDefault();
+		var queryString = $(this).serialize();
+		var selected = $("select option:selected").val();
+		var url = actions[selected];
+		$.ajax({
+			url: url,
+			type: 'GET',
+			data: queryString,
+			success: function(response) {
+				$("#result").html(JSON.stringify(response));
+			},
+			error: function(xhr, textStatus) {
+				$("#result").html(textStatus + "!");
+			}
+		});
+	});			
 
-	});
-</script>
+});
